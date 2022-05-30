dd28639baa1cc12c751aec1ebb3dc84687769fd6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2022-05-30 13:10:56 +0900

    fix: change the indent of registry.json to avoid conflict

--- generate-registry.sh
+++ generate-registry.sh
@@ -7,4 +7,4 @@ cp registry-header.yaml registry.yaml
 # shellcheck disable=SC2094
 find pkgs -name registry.yaml | sort | xargs cat |
 	grep -v -E "^packages:" | grep -v -E "^---$" >> registry.yaml
-aqua -c aqua-dev.yaml exec -- yaml2json registry.yaml > registry.json
+aqua -c aqua-dev.yaml exec -- yaml2json -i "  " registry.yaml > registry.json
