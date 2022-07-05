6a6e36edcbab54464d4f31a8030ee542bd1a8b15
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-07-05 14:24:19 +0900

    fix: generate-registry.sh doesn't output the registry in the same order of pkgs
    Fix #4603

--- generate-registry.sh
+++ generate-registry.sh
@@ -3,6 +3,9 @@
 set -eu
 set -o pipefail
 
+# Make sure sort output the same order on different environments
+export LC_COLLATE=C
+
 cp registry-header.yaml registry.yaml
 # shellcheck disable=SC2094
 find pkgs -name registry.yaml | sort | xargs cat |
