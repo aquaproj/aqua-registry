952d0143af3d96ef87de893703c7c85780fc37f0
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-10-24 20:32:36 +0900

    chore: fix the task scaffold

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -6,8 +6,12 @@ pkg=$1
 
 bash scripts/start.sh
 
-rm -R "pkgs/$pkg"
+if [ -d "pkgs/$pkg" ]; then
+	rm -R "pkgs/$pkg"
+fi
+
 docker exec -ti -w /aqua-registry aqua-registry aqua policy allow
 docker exec -ti -w /aqua-registry aqua-registry aqua i -l
 docker exec -ti -w /aqua-registry aqua-registry aqua-registry scaffold "$pkg"
+
 cmdx t "$pkg"
