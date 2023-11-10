2761c98f0b12e40bafcd787a864d99b6d1d1f07d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-11-10 18:22:23 +0900

    style: fix indent

--- scripts/is_build_image.sh
+++ scripts/is_build_image.sh
@@ -4,11 +4,11 @@ set -eu
 
 if ! docker inspect aquaproj/aqua-registry >/dev/null; then
 	# image doesn't exist
-  exit 1
+	exit 1
 fi
 
 if [ ! -f .build/Dockerfile ]; then
-  exit 1
+	exit 1
 fi
 
 diff -q docker/Dockerfile .build/Dockerfile
