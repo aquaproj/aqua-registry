3f9e5bb00ebb5c5a8c6f669d22860016d13cfcf9
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2023-11-16 14:38:00 +0900

    chore: fix typo (#17418)

--- scripts/is_container_running.sh
+++ scripts/is_container_running.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-echo "[INFO] Checking if the container aqua-regigistry is running" >&2
+echo "[INFO] Checking if the container aqua-registry is running" >&2
 docker ps -a \
 	--filter "name=aqua-registry" \
 	--filter status=running \
