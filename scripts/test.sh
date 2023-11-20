81a7b5139476ac254c4cb8d069295967126f2063
hituzi no sippo <dev@hituzi-no-sippo.me>
2023-11-20 20:51:38 +0900

    test(cargo): fix error
    Error message: `executable files aren't found Files in the unarchived package`

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,20 @@
 set -euo pipefail
 
 pkg=$1
+registry_path="pkgs/$pkg/registry.yaml"
 
 docker cp "pkgs/$pkg/pkg.yaml" aqua-registry:/workspace/pkg.yaml
-docker cp "pkgs/$pkg/registry.yaml" aqua-registry:/workspace/registry.yaml
+docker cp "$registry_path" aqua-registry:/workspace/registry.yaml
+
+set +e
+exists_cargo_type="$(grep --count --max-count=1 '^\s\+type: cargo$' "$registry_path")"
+set -e
 
 for os in linux darwin windows; do
 	for arch in amd64 arm64; do
+		if [[ "$exists_cargo_type" == "1" ]]; then
+			docker exec aqua-registry env AQUA_GOOS="$os" AQUA_GOARCH="$arch" aqua remove "$pkg"
+		fi
 		if ! docker exec aqua-registry env AQUA_GOOS="$os" AQUA_GOARCH="$arch" aqua i; then
 			echo "[ERROR] Build failed $os/$arch" >&2
 			docker exec -ti aqua-registry env AQUA_GOOS="$os" AQUA_GOARCH="$arch" bash
