df44c82f9fac8f355abc5b3e423e22fa7cc5bab8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2023-12-04 10:42:22 +0900

    chore: fix the symbolic link to aqua-proxy when connecting to the container

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -14,4 +14,9 @@ if [ "$OS" = windows ]; then
 	container=aqua-registry-windows
 fi
 echo "[INFO] Connecting to the container $container ($OS/$ARCH)" >&2
+
+# Workaround to fix the symbolic link to aqua-proxy
+# https://github.com/aquaproj/aqua-registry/pull/17896#issuecomment-1837703289
+docker exec "$container" env AQUA_GOOS="$OS" AQUA_GOARCH="$ARCH" aqua i -l
+
 docker exec -ti "$container" env AQUA_GOOS="$OS" AQUA_GOARCH="$ARCH" bash
