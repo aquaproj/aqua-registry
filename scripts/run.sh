8e10466041eadfcac1a7ec358b2f5f619ed12609
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-01-30 09:31:51 +0900

    style: format

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,8 +6,8 @@ container_name=${1:-aqua-registry}
 
 token="${AQUA_GITHUB_TOKEN:-${GITHUB_TOKEN:-}}"
 if [ -z "$token" ]; then
-  echo "[INFO] Get a GitHub Access token by gh auth token" >&2
-  token=$(aqua exec -- gh auth token)
+	echo "[INFO] Get a GitHub Access token by gh auth token" >&2
+	token=$(aqua exec -- gh auth token)
 fi
 envs=""
 if [ -n "$token" ]; then
