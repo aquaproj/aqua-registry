a557d47cfd6eb42330f691f0ad1db524056764cb
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-05-10 11:01:04 +0900

    fix(scripts): use user from host when running docker

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,7 +2,12 @@
 
 set -eu
 
+opts=""
+if [ "$(uname)" = Linux ]; then
+	opts="--build-arg USERNAME=$(id -u -n) --build-arg GROUPNAME=$(id -g -n) --build-arg HOME=/home/$(id -u -n)"
+fi
+
 cp aqua-policy.yaml docker
-docker build -t aquaproj/aqua-registry docker
+docker build -t aquaproj/aqua-registry $opts docker
 mkdir -p .build
 cp docker/Dockerfile .build
