aae79e92d467bb810348e540ad9543e3679e66c6
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-05-23 23:15:42 +0900

    chore(scaffold): validate changes and commit signing before creating a branch (#23192)

--- scripts/check_diff_package.sh
+++ scripts/check_diff_package.sh
@@ -3,17 +3,17 @@
 set -eu
 
 if ! git diff --quiet pkgs registry.yaml; then
-    echo "[ERROR] The directory pkgs and registry.yaml have changes. Please commit changes before running cmdx new" >&2
+    echo "[ERROR] The directory pkgs or registry.yaml has changes." >&2
     git diff --name-only --exit-code pkgs registry.yaml
 fi
 
 if ! git diff --cached --quiet pkgs registry.yaml; then
-    echo "[ERROR] The directory pkgs and registry.yaml have changes. Please commit changes before running cmdx new" >&2
+    echo "[ERROR] The directory pkgs or registry.yaml has changes." >&2
     git diff --cached --name-only --exit-code pkgs registry.yaml
 fi
 
 if [ -n "$(git ls-files --others --exclude-standard pkgs)" ]; then
-    echo "[ERROR] The directory pkgs and registry.yaml have changes. Please commit changes before running cmdx new" >&2
+    echo "[ERROR] The directory pkgs or registry.yaml has changes." >&2
     git ls-files --others --exclude-standard pkgs
     exit 1
 fi
