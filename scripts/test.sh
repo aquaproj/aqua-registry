883ff048362fb6833feed8feb81f1817a245d112
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-05-23 22:55:43 +0900

    chore: stop connecting to a container when test fails (#23191)

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,7 +13,7 @@ for os in linux darwin; do
 		docker exec "$container_name" bash -c "rm aqua-checksums.json 2>/dev/null || :"
 		if ! docker exec "$container_name" env AQUA_GOOS="$os" AQUA_GOARCH="$arch" aqua i; then
 			echo "[ERROR] Build failed $os/$arch" >&2
-			docker exec -ti "$container_name" env AQUA_GOOS="$os" AQUA_GOARCH="$arch" bash
+			echo "        If you want to look into the container, please run 'cmdx con $os $arch'" >&2
 			exit 1
 		fi
 	done
