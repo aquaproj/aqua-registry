f25969447ee2cc22e385436b0626f04f1cf5c3d2
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2024-11-26 09:12:43 +0900

    ci: disable the check of GPG signing (#29213)

--- scripts/check_gpgsign.sh
+++ scripts/check_gpgsign.sh
@@ -2,8 +2,10 @@
 
 set -eu
 
-if [ "$(git config commit.gpgSign)" != true ]; then
-    echo "[ERROR] This repository requires commit signing, so please configure it." >&2
-    echo "        https://github.com/suzuki-shunsuke/oss-contribution-guide/blob/main/docs/commit-signing.md" >&2
-    exit 1
-fi
+# Disable the check https://github.com/suzuki-shunsuke/oss-contribution-guide/issues/77
+
+# if [ "$(git config commit.gpgSign)" != true ]; then
+#     echo "[ERROR] This repository requires commit signing, so please configure it." >&2
+#     echo "        https://github.com/suzuki-shunsuke/oss-contribution-guide/blob/main/docs/commit-signing.md" >&2
+#     exit 1
+# fi
