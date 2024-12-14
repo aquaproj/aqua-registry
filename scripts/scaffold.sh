8788456f5f013122fcddb052ab57e3ac94e459ba
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-15 07:19:05 +0900

    chore: add language server comment

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -14,9 +14,10 @@ if [ -n "$limit" ]; then
 	opts="$opts -limit $limit"
 fi
 
+mkdir -p "pkgs/$pkg"
 # shellcheck disable=SC2086
 docker exec -ti -w /workspace aqua-registry bash -c "rm pkg.yaml 2>/dev/null || :"
-docker exec -ti -w /workspace aqua-registry bash -c "aqua gr $opts --out-testdata pkg.yaml \"$pkg\" > registry.yaml"
-mkdir -p "pkgs/$pkg"
+docker exec -ti -w /workspace aqua-registry bash -c "echo '# yaml-language-server: \$schema=https://raw.githubusercontent.com/aquaproj/aqua/main/json-schema/registry.json' > registry.yaml"
+docker exec -ti -w /workspace aqua-registry bash -c "aqua gr $opts --out-testdata pkg.yaml \"$pkg\" >> registry.yaml"
 docker cp "aqua-registry:/workspace/pkg.yaml" "pkgs/$pkg/pkg.yaml"
 docker cp "aqua-registry:/workspace/registry.yaml" "pkgs/$pkg/registry.yaml"
