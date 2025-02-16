37bd53ba0d07b33297034ed107fe1e670b4af5d1
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-02-16 16:30:15 +0900

    chore: fix scaffold

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -15,10 +15,13 @@ if [ -n "$limit" ]; then
 	opts="$opts -limit $limit"
 fi
 if [ -n "$config" ]; then
-	opts="$opts -c $config"
+	opts="$opts -c aqua-generate-registry.yaml"
 fi
 
 mkdir -p "pkgs/$pkg"
+if [ -n "$config" ];then
+	docker cp "$config" "aqua-registry:/workspace/aqua-generate-registry.yaml"
+fi
 # shellcheck disable=SC2086
 docker exec -ti -w /workspace aqua-registry bash -c "rm pkg.yaml 2>/dev/null || :"
 docker exec -ti -w /workspace aqua-registry bash -c "echo '# yaml-language-server: \$schema=https://raw.githubusercontent.com/aquaproj/aqua/main/json-schema/registry.json' > registry.yaml"
