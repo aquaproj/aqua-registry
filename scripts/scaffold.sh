e074a4fcf8e4b33d34dabf673a48ac3e06947024
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-02-16 19:32:55 +0900

    chore: add scaffold.yaml

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -14,13 +14,17 @@ fi
 if [ -n "$limit" ]; then
 	opts="$opts -limit $limit"
 fi
-if [ -n "$config" ]; then
-	opts="$opts -c aqua-generate-registry.yaml"
+if [ -n "$config" ] || [ -f "pkgs/$pkg/scaffold.yaml" ]; then
+	opts="$opts -c scaffold.yaml"
 fi
 
 mkdir -p "pkgs/$pkg"
 if [ -n "$config" ];then
-	docker cp "$config" "aqua-registry:/workspace/aqua-generate-registry.yaml"
+	docker cp "$config" "aqua-registry:/workspace/scaffold.yaml"
+fi
+if [ -z "$config" ] && [ -f "pkgs/$pkg/scaffold.yaml" ]; then
+	echo "[INFO] Using pkgs/$pkg/scaffold.yaml" >&2
+	docker cp "pkgs/$pkg/scaffold.yaml" "aqua-registry:/workspace/scaffold.yaml"
 fi
 # shellcheck disable=SC2086
 docker exec -ti -w /workspace aqua-registry bash -c "rm pkg.yaml 2>/dev/null || :"
@@ -28,3 +32,6 @@ docker exec -ti -w /workspace aqua-registry bash -c "echo '# yaml-language-serve
 docker exec -ti -w /workspace aqua-registry bash -c "aqua gr $opts --out-testdata pkg.yaml \"$pkg\" >> registry.yaml"
 docker cp "aqua-registry:/workspace/pkg.yaml" "pkgs/$pkg/pkg.yaml"
 docker cp "aqua-registry:/workspace/registry.yaml" "pkgs/$pkg/registry.yaml"
+if [ -n "$config" ];then
+	cp "$config" "pkgs/$pkg/scaffold.yaml"
+fi
